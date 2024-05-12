7f18d9ba5d2f946aa14100095a57112da4b8c580
Ypatios Chaniotakos <ypatioschaniotakos@gmail.com>
2024-05-12 14:08:20 +0300

    initializing app

---
+++ pkg/render/render.go
@@ -0,0 +1,84 @@
+package render
+
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+	"text/template"
+
+	"github.com/YpatiosCh/bookings/pkg/config"
+	"github.com/YpatiosCh/bookings/pkg/models"
+)
+
+var app *config.AppConfig
+
+// NewTemplates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
+// renders templates using html template
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+	var tc map[string]*template.Template
+
+	if app.UseCache {
+		//get the template cache from the app config
+		tc = app.TemplateChache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+
+	// get requested template from cache
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("Could not get template from template cache")
+	}
+
+	td = AddDefaultData(td)
+
+	err := t.Execute(w, td)
+	if err != nil {
+		log.Println(err)
+	}
+
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	//myCache := make(map[string]*template.Template) same as below
+	myCahce := map[string]*template.Template{}
+
+	// get all of the files named *.page.tmpl from ./templates
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCahce, err
+	}
+
+	// range through all files  *.page.tmpl
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCahce, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCahce, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCahce, err
+			}
+		}
+
+		myCahce[name] = ts
+	}
+
+	return myCahce, nil
+}
