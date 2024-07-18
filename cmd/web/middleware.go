19235bda56542eddfadd1f05d02e7656b4a2ccf1
Ypatios Chaniotakos <ypatioschaniotakos@gmail.com>
2024-07-18 14:52:22 +0300

    final commit after 25000000 hours of fucking fuck

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/justinas/nosurf"
+	"github.com/tsawler/bookings/internal/helpers"
+	"net/http"
 )
 
 // NoSurf adds CSRF protection to all POST requests
@@ -16,11 +16,21 @@ func NoSurf(next http.Handler) http.Handler {
 		Secure:   app.InProduction,
 		SameSite: http.SameSiteLaxMode,
 	})
-
 	return csrfHandler
 }
 
-// SessionLoad loads ands saves the session on every request
+// SessionLoad loads and saves the session on every request
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
+}
+
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !helpers.IsAuthenticated(r) {
+			session.Put(r.Context(), "error", "Log in first!")
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
